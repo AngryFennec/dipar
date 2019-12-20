c610c76ecc84de91b58fdf92d9a166531fd6ff5e
AngryFennec <shellfix@gmail.com>
2019-12-20 12:40:26 +0300

    слайдер

--- src/js/modules/landing.js
+++ src/js/modules/landing.js
@@ -2,41 +2,23 @@
 
 (function() {
 var pricesSwiper;
+var pricesSelector = document.querySelectorAll('.prices__swiper');
+if (pricesSelector) {
 
     // function initSwiper() {
     //     console.log('12414125412512512');
     //     if(document.body.clientWidth <= 1240) {
             pricesSwiper = new Swiper('.prices__swiper', {
-                // slidesPerView: 5,
-                // spaceBetween: 9,
+                 slidesPerView: 'auto',
+                 spaceBetween: 9,
                 // centeredSlides: true,
                   navigation: {
                     nextEl: '.prices__next-button',
                     prevEl: '.prices__prev-button',
                   },
-                breakpoints: {
-                    2000: {
-                        slidesPerView: 5,
-                        spaceBetween: 9
-                    },
-                    1920: {
-                        slidesPerView: 5,
-                        spaceBetween: 9
-                    },
-                    1100: {
-                        slidesPerView: 4,
-                        spaceBetween: 9
-                    },
-                    768: {
-                        slidesPerView: 3,
-                        spaceBetween: 9
-                    },
-                    500: {
-                        slidesPerView: 2,
-                        spaceBetween: 9
-                    }
-                },
+
             });
+          }
 
 //             return pricesSwiper;
 //         } else {
@@ -46,11 +28,11 @@ var pricesSwiper;
 //         }
 
 //     }
-    
+
 //     initSwiper();
 
 //     window.addEventListener('resize', function() {
 //         setTimeout(initSwiper, 500)
 //     })
-    
+
 })();
